f203fd8642c88d454d35efdd5c7053d5e8901dd3
Kader-Goni <kadergoni35@gmail.com>
2022-05-27 12:04:22 +0600

    NoT Found Route edited

---
+++ src/Pages/Shared/Notfound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Notfound = () => {
+  return (
+    <div className="mx-4 md:mx-8 lg:mx-16 mt-24">
+      <div className="border border-accent border-x-4 w-full md:w-[50%] mx-auto text-center rounded-xl p-3">
+        <h1 className="text-2xl text-accent">Opps... Page NOT FOUND... 404 errors...</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Notfound;
